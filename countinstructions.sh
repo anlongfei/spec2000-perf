618b46205c9346c362d295cb60f90023ce06cdc4
longfei <longfei2015@iscas.ac.cn>
2017-01-05 12:01:26 +0800

    ADD: countinstrs

---
+++ countinstructions.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+specDir=/home/alf/spec2000/CPU2000_install
+curDir=`pwd`
+
+INT="164.gzip     
+175.vpr    
+176.gcc    
+181.mcf    
+186.crafty 
+197.parser 
+252.eon    
+254.gap    
+253.perlbmk
+255.vortex
+256.bzip2 
+300.twolf"
+
+FP="168.wupwise 
+171.swim    
+172.mgrid   
+173.applu   
+177.mesa    
+178.galgel  
+179.art     
+183.equake  
+187.facerec 
+188.ammp    
+189.lucas   
+191.fma3d   
+200.sixtrack
+301.apsi"
+
+cd $specDir
+source shrc
+cd $curDir
+echo "./RunAndKill.sh"
+sleep 1
+./RunAndKill.sh
+
+if [ -d "instrs" ];then
+	rm -rfv instrs/*
+else
+	mkdir instrs
+fi
+inthead=${specDir}/benchspec/CINT2000/
+fphead=${specDir}/benchspec/CFP2000/
+tail=/run/00000002
+
+echo "****************************int*******************************" > ${curDir}/instrs/int.instr
+for case in $INT
+do
+	casedir=${inthead}${case}${tail}
+	echo $casedir
+	cd $casedir
+	echo $case >>  ${curDir}/instrs/int.instr
+	#perf stat -e instructions:u ls 2>> ${curDir}/instrs/int.instr
+	sed -i '3,$d' speccmds.cmd
+	perf stat -e instructions:u specinvoke speccmds.cmd  2>> ${curDir}/instrs/int.instr
+done
+
+
+echo "****************************fp*******************************" > ${curDir}/instrs/fp.instr
+for case in $FP
+do
+	casedir=${fphead}${case}${tail}
+	echo $casedir
+	cd $casedir
+	echo $case >>  ${curDir}/instrs/fp.instr
+	#perf stat -e instructions:u ls 2>> ${curDir}/instrs/fp.instr
+	perf stat -e instructions:u specinvoke speccmds.cmd  2>> ${curDir}/instrs/fp.instr
+done
+
---
+++ dealinstrs.py
@@ -0,0 +1,101 @@
+#!/usr/bin/python
+#-* coding:utf-8 -*-
+list_fp = [
+    "168.wupwise"
+    ,"171.swim"
+    ,"172.mgrid"
+    ,"173.applu"
+    ,"177.mesa"
+    ,"178.galgel"
+    ,"179.art"
+    ,"183.equake"
+    ,"187.facerec"
+    ,"188.ammp"
+    ,"189.lucas"
+    ,"191.fma3d"
+    ,"200.sixtrack"
+    ,"301.apsi" ]
+list_int = [
+    "164.gzip"
+    ,"175.vpr"
+    ,"176.gcc"
+    ,"181.mcf"
+    ,"186.crafty"
+    ,"197.parser"
+    ,"252.eon"
+    ,"254.gap"
+    ,"253.perlbmk"
+    ,"255.vortex"
+    ,"256.bzip2"
+    ,"300.twolf"
+    ]
+global fint
+global ffp
+global output
+
+def dealdata():
+    global fint
+    global ffp
+    global output
+    print "** dealdata**"
+    int = []
+    fp = []
+
+    f = open(fint)
+    lines = f.readlines()
+    for line in lines:
+        words = line.split()
+        len_words = len(words)
+        if len_words == 2 or len_words == 1:
+            int.append(words)
+    f.close()
+
+    f = open(ffp)
+    lines = f.readlines()
+    for line in lines:
+        words = line.split()
+        len_words = len(words)
+        if len_words == 2 or len_words == 1:
+            fp.append(words)
+    f.close()
+
+    outfile = output+"/int.xlsx"
+    print outfile
+    f=open(outfile,"wb")
+    print "===========int======================"
+    for case in int:
+        print case
+        lencase = len(case)
+        if lencase == 1:
+            f.write(case[0]+"\n")
+        if lencase == 2:
+            f.write(case[1]+"\t"+case[0]+"\n")
+    f.close()
+
+    outfile=output+"/fp.xlsx"
+    f=open(outfile,"wb")
+    print "===========fp======================"
+    for case in fp:
+        lencase = len(case)
+        if lencase == 1:
+            f.write(case[0]+"\n")
+        if lencase == 2:
+            f.write(case[1]+"\t"+case[0])
+    f.close()
+
+
+def main():
+    init()
+    dealdata()
+
+def init():
+    global fint
+    global ffp
+    global output
+    print "init"
+    fint ="./instrs/int.instr"
+    ffp ="./instrs/fp.instr"
+    output="./instrs"
+
+if __name__ == "__main__":
+    main()
